03226c03fc688e750d167d3181faa827e8ee3eed
halilatilla <atillahalil0@gmail.com>
2021-02-14 22:06:20 +0300

    contact page seo added

--- pages/contact.js
+++ pages/contact.js
@@ -1,14 +1,23 @@
 import React from 'react'
+import { NextSeo } from 'next-seo'
 
+import SeoConfig from '../next-seo.config'
 import { ContactForm } from '@/components/index'
 
 export default function contact() {
+  const SEO = {
+    title: `${SeoConfig.title} | Contact`,
+  }
+
   return (
-    <section className="contact">
-      <div className="contact__bg" />
-      <div className="contact__box container">
-        <ContactForm />
-      </div>
-    </section>
+    <>
+      <NextSeo {...SEO} />
+      <section className="contact">
+        <div className="contact__bg" />
+        <div className="contact__box container">
+          <ContactForm />
+        </div>
+      </section>
+    </>
   )
 }
