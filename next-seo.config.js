372841926ee5bd4e30b3631484563f9b53163ffd
halilatilla <atillahalil0@gmail.com>
2021-02-15 19:03:58 +0300

    next-seo updated

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,10 +1,11 @@
 export default {
   title: 'Basic Login',
   description: 'Basic Login App',
+  url: 'http://basic-login-i18n.vercel.app/',
   openGraph: {
     type: 'website',
     locale: 'tr_TR',
-    url: '',
+    url: 'http://basic-login-i18n.vercel.app/',
     site_name: 'Basic Login',
   },
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
 
+import SiteConfig from '../next-seo.config'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -11,35 +13,46 @@ class MyDocument extends Document {
     return (
       <Html lang="tr">
         <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="180x180"
-            href="/favicons/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/favicons/favicon-16x16.png"
-          />
-          <link rel="manifest" href="/favicons/site.webmanifest" />
+          {/* base */}
+          <meta charSet="utf-8" />
+          <meta name="robots" content="follow, index" />
+          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+          <meta name="description" content={SiteConfig.description} />
+
+          {/* facebook */}
+          <meta property="og:url" content={SiteConfig.url} />
+          <meta property="og:type" content="website" />
+          <meta property="og:title" content={SiteConfig.title} />
+          <meta property="og:description" content={SiteConfig.description} />
+
+          {/* twitter */}
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:url" content={SiteConfig.url} />
+          <meta name="twitter:title" content={SiteConfig.title} />
+          <meta name="twitter:description" content={SiteConfig.description} />
 
           {/* pwa */}
-          <link rel="manifest" href="/favicons/site.webmanifest" />
-          <meta name="theme-color" content="#fff" />
-          <meta name="application-name" content="Basic Login" />
-          <meta name="apple-mobile-web-app-title" content="Basic Login" />
-          <meta name="apple-mobile-web-app-capable" content="yes" />
-          <meta
-            name="apple-mobile-web-app-status-bar-style"
-            content="default"
+          <link href="favicons/site.webmanifest" rel="manifest" />
+          <link
+            href="favicons/apple-touch-icon.png"
+            rel="apple-touch-icon"
+            sizes="180x180"
           />
+          <link
+            href="favicons/favicon-32x32.png"
+            rel="icon"
+            sizes="32x32"
+            type="image/png"
+          />
+          <link
+            href="favicons/favicon-16x16.png"
+            rel="icon"
+            sizes="16x16"
+            type="image/png"
+          />
+
+          <meta content="#ffffff" name="theme-color" />
+          <meta content="#ffffff" name="msapplication-TileColor" />
         </Head>
         <body>
           <Main />
