f229a134cd8469f7349084e8e8fb2eabe9513621
halilatilla <atillahalil0@gmail.com>
2021-02-15 18:44:08 +0300

    next-pwa added

--- next.config.js
+++ next.config.js
@@ -1,14 +1,27 @@
 const path = require('path')
 const withPlugins = require('next-compose-plugins')
 const withSvgr = require('next-svgr')
+const withPWA = require('next-pwa')
 const { nextI18NextRewrites } = require('next-i18next/rewrites')
 
-module.exports = withPlugins([
-  { rewrites: async () => nextI18NextRewrites() },
-  withSvgr({
-    includeFileLoader: true,
-    svgrOptions: {
-      configFile: path.resolve(__dirname, 'svgr.config.js'),
-    },
-  }),
-])
+const isDev = process.env.NODE_ENV !== 'production'
+
+const nextConfig = {
+  pwa: {
+    dest: 'public',
+    disable: isDev,
+  },
+}
+
+module.exports = withPlugins(
+  [
+    { rewrites: async () => nextI18NextRewrites() },
+    withSvgr({
+      includeFileLoader: true,
+      svgrOptions: {
+        configFile: path.resolve(__dirname, 'svgr.config.js'),
+      },
+    }),
+  ],
+  withPWA(nextConfig)
+)
--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import React from 'react'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx);
-    return { ...initialProps };
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
   }
 
   render() {
@@ -29,14 +29,25 @@ class MyDocument extends Document {
             href="/favicons/favicon-16x16.png"
           />
           <link rel="manifest" href="/favicons/site.webmanifest" />
+
+          {/* pwa */}
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <meta name="theme-color" content="#fff" />
+          <meta name="application-name" content="Basic Login" />
+          <meta name="apple-mobile-web-app-title" content="Basic Login" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="default"
+          />
         </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
